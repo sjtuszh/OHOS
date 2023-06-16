488b00fdeee106cf7eaf2a86d02afa92dc056e7e
sjtuszh <96711173+sjtuszh@users.noreply.github.com>
2023-06-16 22:01:53 +0800

    Initial

---
+++ ArkTSdemo/entry/src/main/java/com/szh/arktsdemo/MyApplication.java
@@ -0,0 +1,10 @@
+package com.szh.arktsdemo;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
