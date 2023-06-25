2bb44ea7951d57506ed7631c781469a7c41e871c
sjtuszh <96711173+sjtuszh@users.noreply.github.com>
2023-06-25 18:21:52 +0800

    v1.2 with tablist bind slider

--- ICT/distributor/entry/src/main/java/com/szh/distributor/slice/MainAbilitySlice.java
+++ ICT/distributor/entry/src/main/java/com/szh/distributor/slice/MainAbilitySlice.java
@@ -4,6 +4,8 @@ import com.szh.distributor.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import ohos.agp.components.*;
+import ohos.agp.render.opengl.Utils;
+import ohos.agp.utils.TextAlignment;
 import ohos.hiviewdfx.HiLog;
 import ohos.hiviewdfx.HiLogLabel;
 
@@ -13,12 +15,72 @@ public class MainAbilitySlice extends AbilitySlice {
     static final HiLogLabel ability = new HiLogLabel(HiLog.LOG_APP, 0x00201, "MainAbilitySlice");
     private TabList mtabList;
 
+    // PagerSlider对象
+    private PageSlider mPagerSlider;
+    // 需要PagerSlider对象管理的用户界面列表
+    private ArrayList<Component> mPageview;
 
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
 
+        // 获取PagerSlider对象
+        mPagerSlider = (PageSlider) findComponentById(ResourceTable.Id_pager_slider);
+
+        // 创建PagerSlider所需要承载界面的列表
+        mPageview = new ArrayList<Component>();
+        mPageview.add(generateTextComponent("第1个界面"));
+        mPageview.add(generateTextComponent("第2个界面"));
+        mPageview.add(generateTextComponent("第3个界面"));
+        // 实例化PageSliderProvider，为PagerSlider提供界面
+        mPagerSlider.setProvider(new PageSliderProvider() {
+            @Override
+            public int getCount() {
+                // 界面数量
+                return mPageview.size();
+            }
+
+            @Override
+            public Object createPageInContainer(ComponentContainer componentContainer, int i) {
+                HiLog.info(ability,String.format("createPageInContainer%d",i));
+                // 添加用户界面
+                componentContainer.addComponent(mPageview.get(i));
+                return mPageview.get(i);
+            }
+
+            @Override
+            public void destroyPageFromContainer(ComponentContainer componentContainer, int i, Object o) {
+                HiLog.info(ability,String.format("destroyPageFromContainer%d",i));
+                // 添加用户界面
+                componentContainer.removeComponent(mPageview.get(i));
+            }
+
+            @Override
+            public boolean isPageMatchToObject(Component component, Object o) {
+                // 判断createPageInContainer创建的界面与当前界面是否相同
+                return component == o;
+            }
+        });
+        //先进入设备界面
+        mPagerSlider.setCurrentPage(1);
+
+        mPagerSlider.addPageChangedListener(new PageSlider.PageChangedListener() {
+            @Override
+            public void onPageSliding(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSlideStateChanged(int i) {
+
+            }
+
+            @Override
+            public void onPageChosen(int i) {
+                mtabList.selectTabAt(i);
+            }
+        });
 
         //初始化TabList
         mtabList = (TabList) findComponentById(ResourceTable.Id_tab_list);
@@ -35,6 +97,25 @@ public class MainAbilitySlice extends AbilitySlice {
         }
         mtabList.selectTabAt(1);
 
+        mtabList.addTabSelectedListener(new TabList.TabSelectedListener() {
+            @Override
+            public void onSelected(TabList.Tab tab) {
+                HiLog.info(ability,String.format("onSlected%d",tab.getTag()));
+                mPagerSlider.setCurrentPage((int)tab.getTag());
+            }
+
+            @Override
+            public void onUnselected(TabList.Tab tab) {
+
+            }
+
+            @Override
+            public void onReselected(TabList.Tab tab) {
+
+            }
+        });
+
+
 
     }
 
@@ -50,5 +131,13 @@ public class MainAbilitySlice extends AbilitySlice {
     }
 
 
+    private Text generateTextComponent(String content) {
+        Text text = new Text(this);
+        text.setLayoutConfig(new ComponentContainer.LayoutConfig(ComponentContainer.LayoutConfig.MATCH_PARENT, ComponentContainer.LayoutConfig.MATCH_PARENT));
+        text.setTextAlignment(TextAlignment.CENTER);
+        text.setText(content);
+        text.setTextSize(80);
+        return text;
+    }
 }
 
